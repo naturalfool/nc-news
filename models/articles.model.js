2aaff60296a43043bd9e2b9258d328779c8e3066
Josh Rigg <joshrigg97@gmail.com>
2023-11-24 14:26:40 +0000

    finish articles sort_by order task

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,20 +1,5 @@
 const db = require("../db/connection");
 
-// exports.selectArticleById = (article_id) => {
-//   return db
-//     .query(
-//       `SELECT * FROM articles
-//                 WHERE article_id = $1;`,
-//       [article_id]
-//     )
-//     .then((article) => {
-//       if (article.rows.length === 0) {
-//         return Promise.reject({ status: 404, msg: "Article not found" });
-//       } else {
-//         return article;
-//       }
-//     });
-// };
 exports.selectArticleById = (article_id) => {
   return db
     .query(
@@ -68,8 +53,6 @@ queryValues.push(order)
   return db.query(queryStr, queryValues)
 }
 
-
-  // return db.query(queryStr, queryValues)
    
 };
 
