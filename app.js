5dc5b741a5b9e5a7fd831f1f8f956c2fe6b0220c
Josh Rigg <joshrigg97@gmail.com>
2023-12-11 09:54:38 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const { getAllTopics } = require("./controllers/topics.controller")
 const { getAPIEndpoints } = require("./controllers/api.controller")
 const { getArticleById, getAllArticles, patchArticleVotesById } = require("./controllers/articles.controller");
 const { handlePostgresErrors, handleCustomErrors, handleServerErrors } = require("./errors");
-const { getCommentsByArticleId, postCommentByArticleId, deleteCommentById } = require("./controllers/comments.controller")
-const { getUsers } = require("./controllers/users.controller")
+const { getCommentsByArticleId, postCommentByArticleId, deleteCommentById, patchCommentVotesById } = require("./controllers/comments.controller")
+const { getUsers, getUserByUsername } = require("./controllers/users.controller")
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/topics", getAllTopics)
@@ -15,10 +17,12 @@ app.get("/api/articles", getAllArticles)
 app.get("/api/articles/:article_id", getArticleById)
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 app.get("/api/users", getUsers)
+app.get("/api/users/:username", getUserByUsername)
 
 app.post("/api/articles/:article_id/comments", postCommentByArticleId)
 
 app.patch("/api/articles/:article_id", patchArticleVotesById)
+app.patch("/api/comments/:comment_id", patchCommentVotesById)
 
 app.delete("/api/comments/:comment_id", deleteCommentById)
 
